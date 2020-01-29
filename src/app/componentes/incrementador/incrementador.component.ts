780b0e7d780ab867d9d04ce0bece69ddd8a55feb
pedroll <pedrollongo@gmail.com>
2020-01-29 19:24:46 +0100

    @ViewChild - correjimos poder tipear valores invalidos y fuera de rango en el input

--- src/app/componentes/incrementador/incrementador.component.ts
+++ src/app/componentes/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
@@ -7,6 +7,8 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 })
 export class IncrementadorComponent implements OnInit {
 
+  // impediamos ver entrads erroneas en el input, referenciamos con # el input al que queremos c ambiar valor
+  @ViewChild('porcentajex', {static: false}) campoInput: ElementRef;
   // en el componente hijo rrecibimos con @Input
   @Input() leyenda = 'Leyenda';
   @Input() porcentaje = 50;
@@ -22,14 +24,40 @@ export class IncrementadorComponent implements OnInit {
 
   cambiarValor(incremento: number) {
 
-    if (this.porcentaje >= 100 && incremento > 0
-      ||
+    if (this.porcentaje >= 100 && incremento > 0) {
+      this.porcentaje = 100;
+      this.cambioValor.emit(this.porcentaje);
+
+      return;
+    }
+    if (
       this.porcentaje <= 0 && incremento < 0) {
+      this.porcentaje = 0;
+      this.cambioValor.emit(this.porcentaje);
+
       return;
     }
     this.porcentaje = this.porcentaje + incremento;
 
     // tras actualizar el valor emitimos el evento
     this.cambioValor.emit(this.porcentaje);
+  }
+
+  enCambio(event: number) {
+    // console.log(event);
+    // const elementoHTML: any = document.getElementsByName('porcentaje')[0];
+    if (event >= 100) {
+      this.porcentaje = 100;
+    } else if (event <= 0) {
+      this.porcentaje = 0;
+    } else {
+      this.porcentaje = event;
+    }
+
+    // elementoHTML.value = this.porcentaje;
+    this.campoInput.nativeElement.value = this.porcentaje;
+
+    this.cambioValor.emit(this.porcentaje);
+
   }
 }
