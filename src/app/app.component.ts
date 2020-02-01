e062618ff9fe484b930864bd8574c367c964d102
pedroll <pedrollongo@gmail.com>
2020-02-01 14:06:09 +0100

    Uso de scripts externos a angular importados en index.html. Como plugins, jquery...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import {Component} from '@angular/core';
 import {SettingsService} from './services/settings.service';
 
+// declaramos la funcion qque ya existe en el archivo importado en index.html. pero externo a Angular
+declare function iniciar_plugins();
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +11,8 @@ import {SettingsService} from './services/settings.service';
 })
 export class AppComponent {
   constructor(public settings: SettingsService) {
+    // iniciamos la funcion que hemos creado con el script externo
+    iniciar_plugins();
   }
 
   title = 'adminPro';
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public  router: Router) {
   }
 
+  ngOnInit() {
+
+
+  }
+
+  ingresar() {
+    console.log('Ingresando');
+    this.router.navigate(['/dashboard']);
+  }
 }
